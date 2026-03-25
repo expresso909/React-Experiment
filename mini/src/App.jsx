239ed251ab18ed23277ac87c5e384b9a9ae03764
Sidhartha S Sahoo <githubexpresso@gmail.com>
2026-03-25 18:02:42 +0530

    01-mini

---
+++ mini/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <div>Randi ke sapouto!</div>
+    <div id ='quote'>Mahatma Ghandu </div>
+
+    </>
+  )
+}
+
+export default App
